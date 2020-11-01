58aab7673abf0e5af04d02db62479b17131b3479
Erik Axelsson <e.axelsson@tutanota.com>
2020-11-01 17:24:24 +0100

    add comments

--- bugbash-nuxt/static/ErrorMonitor.js
+++ bugbash-nuxt/static/ErrorMonitor.js
@@ -1,4 +1,5 @@
 /**
+ * Monitor for errors and add data to database.
  * 
  * @module ErrorMonitor
  */
@@ -19,18 +20,58 @@ export default class ErrorMonitor {
         window.addEventListener('error', this.onError, true);        
     }
 
+    /**
+     * Stop the wondow monitor for errors.
+     */
     stop() {
         window.removeEventListener('error', this.onError, true);        
     }
 
+    /**
+     * 
+     * @param {errorEvent} errorEvent 
+     */
     onError(errorEvent) { 
         const errorTimeStamp = new Date();
         this.logError(errorEvent.error, errorTimeStamp);
+        this.addError(errorEvent.error);
     }
 
+    /**
+     * Log info about error in the console.
+     * 
+     * @param {error} error 
+     * @param {Date} errorTimeStamp 
+     */
     logError(error, errorTimeStamp) {
         console.log("Error name: " + error.name + ", Error message: " + error.message
         + ", Error file: " + this.errorFile + ", Error time stamp: " + errorTimeStamp);
+    }
+
+    /**
+     * Add error to database.
+     * 
+     * @param {error} error 
+     */
+    addError(error) {
+        console.log('inside addError');
+        errorName = error.name;
+        this.$axios.post( '/server/monitorRoute', {
+            name: errorName
+          })
+          .then((response) => {
+            console.log('inside addError');
+            console.log(response);
+            if(response.data._id){
+              this.$router.push({ name:'monitorRoute', params:{ created:'yes' } })
+            }
+          })
+          .catch( (error) => {
+            console.log(error)
+            if(error.response.data.errors){
+              this.errors = error.response.data.errors
+            }
+          });
     }
 };
 
