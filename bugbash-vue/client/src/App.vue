81e62d2c5c0dda155a8a2d13711ea2d7fef80802
Erik Axelsson <e.axelsson@tutanota.com>
2021-03-10 10:54:42 +0100

    move google sign in and avatar to SignIn component

--- bugbash-vue/client/src/App.vue
+++ bugbash-vue/client/src/App.vue
@@ -6,118 +6,25 @@
         <button><font-awesome-icon icon="bars" /></button>
       </div>
     </header>
-    <GoogleLogin
-      :params="params"
-      :renderParams="renderParams"
-      :onSuccess="onSuccess"
-      :onFailure="onFailure"
-    ></GoogleLogin>
-    <GoogleLogin :params="params" :logoutButton="true" :signOut="signOut"
-      >Logout</GoogleLogin
-    >
-    <!-- <div v-if="this.userIcon !== null">
-      <img :src="userIcon" alt="user icon" />
-    </div> -->
-    <div>
-      <avatar
-        :click="showImageUploader"
-        v-if="this.signedIn == true"
-        :username="`${this.userInitials}`"
-        :src="this.$store.getters.avatarImage"
-        :size="40"
-      ></avatar>
-    </div>
-
-    <image-uploader
-      
-      :debug="1"
-      :maxWidth="512"
-      :quality="0.7"
-      :autoRotate="true"
-      outputFormat="verbose"
-      :preview="false"
-      :className="['fileinput', { 'fileinput--loaded': hasImage }]"
-      capture="environment"
-      accept="image/*"
-      doNotResize="['gif', 'svg']"
-      @input="setImage"
-      @onUpload="startImageResize"
-      @onComplete="endImageResize"
-    >
-      <label for="fileInput" slot="upload-label">
-        <span class="upload-caption">{{
-          hasImage ? "Replace" : "Upload"
-        }}</span>
-      </label>
-    </image-uploader>
-    <button @click="removeUserImage">Remove userimage</button>
+  <sign-in></sign-in>
+  <router-link to="/"></router-link>
   </div>
 </template>
 
 <script>
 import html2canvas from "html2canvas";
-import GoogleLogin from "vue-google-login";
-// import VueGapi from "vue-gapi";
-// Behövs för viss funktionalitet
-// import { LoaderPlugin } from 'vue-google-login';
-
-import Avatar from "vue-avatar";
-import image from "./assets/logo.png";
-// import SimpleUpload from "./components/SimpleUpload.vue";
+import SignIn from "./components/SignIn.vue";
 
 export default {
   name: "App",
   components: {
-    GoogleLogin,
-    Avatar,
-    // SimpleUpload,
+    SignIn,
   },
   data: function() {
     return {
-      output: null,
-      userIcon: null,
-      userInitials: null,
-      signedIn: false,
-      file: "",
-      signedInUser: null,
-      // avatarTest: [],
-      testImage: image,
-      // selectedFile: null,
-      imageUploaderDisplay: false,
-      image: "",
-      hasImage: false,
-      params: {
-        client_id:
-          "239286565520-4olejvir9qtbmtsbdrn82lakb1gls3qp.apps.googleusercontent.com",
-        // client_id: process.env.GOOGLECLIENT_ID,
-      },
-      renderParams: {
-        width: 250,
-        height: 50,
-        longtitle: true,
-      },
     };
   },
-  //  mounted() {
-  //     let recaptchaScript = document.createElement('script')
-  //     recaptchaScript.setAttribute('src', 'https://apis.google.com/js/platform.js')
-  //     document.head.appendChild(recaptchaScript)
-  //   },
   methods: {
-    onSuccess(googleUser) {
-      console.log(googleUser);
-      // console.log(JSON.stringify(googleUser));
-      const data = googleUser.getBasicProfile();
-      console.log("full name: " + data.sd);
-      this.signedInUser = googleUser;
-      this.userInitials = data.sd;
-      this.signedIn = true;
-      // const image = googleUser.getBasicProfile().getImageUrl();
-      // this.userIcon = image;
-      // const test = gapi.auth2.getAuthInstance().isSignedIn.get();
-      // console.log(test);
-      // console.log(auth2.isSignedIn.get());
-    },
     async print() {
       // const el = this.$refs.printMe;
       const el = document.body;
@@ -134,20 +41,6 @@ export default {
         document.body.appendChild(canvas);
       });
     },
-    setImage: function(file) {
-      this.hasImage = true;
-      this.image = file;
-      this.$store.commit("changeAvatarImage", file.dataUrl);
-    },
-    removeUserImage() {
-      this.$store.commit("changeAvatarImage", "");
-    },
-    showImageUploader() {
-      this.imageUploaderDisplay = true;
-    },
-    hideImageUploader() {
-      this.imageUploaderDisplay = false;
-    }
   },
 };
 </script>
