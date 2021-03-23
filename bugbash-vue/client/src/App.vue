ee0dd16ca8e72339b923e7f3e035ab4db51a9850
Erik Axelsson <e.axelsson@tutanota.com>
2021-03-23 17:23:38 +0100

    exchange menubutton styling

--- bugbash-vue/client/src/App.vue
+++ bugbash-vue/client/src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <header class="main-header">
       <a class="logo">Bugbash</a>
-      <div class="global-buttons">
-      </div>
+      <!-- <div class="global-buttons"></div> -->
       <div>
         <avatar
           v-if="this.signedIn == true"
@@ -14,18 +13,14 @@
         ></avatar>
       </div>
 
-      <menu-wrapper>
-        <template slot="menu-button"
-          ><font-awesome-icon icon="bars" />
-        </template>
-        <template slot="menu-content">
-          <menu-link><router-link to="/">Start Page</router-link></menu-link>
-          <menu-link
-            ><router-link to="/user">User Sign In</router-link></menu-link
-          >
-          <menu-link><router-link to="/about">About</router-link></menu-link>
-        </template>
-      </menu-wrapper>
+      <div class="dropdown">
+        <button class="dropbtn"><font-awesome-icon icon="bars" /></button>
+        <div class="dropdown-content">
+          <router-link to="/">Start Page</router-link>
+          <router-link to="/user">User Sign In</router-link>
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
     </header>
 
     <router-view></router-view>
@@ -34,15 +29,12 @@
 
 <script>
 import Avatar from "vue-avatar";
-import { MenuWrapper, MenuLink } from "vue-menu-button";
 // import GoogleLogin from "vue-google-login";
 
 export default {
   name: "App",
   components: {
     Avatar,
-    MenuWrapper,
-    MenuLink,
   },
   data: function() {
     return {
@@ -163,113 +155,62 @@ li {
   margin-right: 2.7em;
 }
 
+.menu-button {
+  background: #333;
+}
+
 /* a {
   color: #42b983;
 } */
+
+.dropbtn {
+  /* background-color: #4caf50; */
+  color: white;
+  padding: 1em;
+  font-size: 1em;
+  border: none;
+}
+
+.dropbtn:hover {
+  color: #388af6;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: rgb(229, 233, 240);
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color:  white;
+}
 
 /* img {
   border: 3px solid black;
   border-radius: 25px;
 } */
 
-[data-vue-menu-button] {
-  background-color: transparent;
-  border: 1px solid #555;
-  color: #555;
-  cursor: pointer;
-  font-size: inherit;
-  display: inline-block;
-  border-radius: 3px;
-  font-weight: 700;
-  height: 40px;
-  line-height: 40px;
-  padding: 0 30px;
-  text-align: center;
-  white-space: nowrap;
-}
-
-[data-vue-menu-button]:focus {
-  outline: 0;
-  color: #236ca4;
-  border-color: #236ca4;
-  box-shadow: 0 0 3px 2px #236ca4;
-}
-
-[data-vue-menu] {
-  list-style: none;
-  margin: 10px 0 0 0;
-  padding: 0;
-  width: 220px;
-  padding: 10px 0;
-  border: 1px solid grey;
-  position: relative;
-  animation: slide-down 0.2s ease;
-}
-
-[data-vue-menu]::before {
-  content: "";
-  width: 0;
-  height: 0;
-  position: absolute;
-  border-left: 7px solid transparent;
-  border-right: 7px solid transparent;
-  border-bottom: 7px solid black;
-  top: -7px;
-  left: 15px;
-}
-
-[data-vue-menu]::after {
-  content: "";
-  width: 0;
-  height: 0;
-  position: absolute;
-  border-left: 6px solid transparent;
-  border-right: 6px solid transparent;
-  border-bottom: 6px solid white;
-  top: -6px;
-  left: 16px;
-}
-
-[data-vue-menu-item],
-[data-vue-menu-link] {
-  color: inherit;
-  text-decoration: none;
-  line-height: 28px;
-  height: 28px;
-  cursor: pointer;
-  padding: 0 10px;
-  display: block;
-}
-
-[data-vue-menu-item]:focus,
-[data-vue-menu-link]:focus {
-  outline: 0;
-  background-color: #236ca4;
-  color: #fff;
-  display: block;
-}
-
-[data-vue-menu-item]:hover,
-[data-vue-menu-link]:hover {
-  outline: 0;
-  background-color: #236ca4;
-  color: #fff;
-  display: block;
-}
-
-[tabindex="-1"]:focus,
-*::-moz-focus-inner {
-  border: 0;
-}
-
-@keyframes slide-down {
-  0% {
-    opacity: 0;
-    transform: translateY(-10px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
 </style>
